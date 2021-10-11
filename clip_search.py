b1c4dd0675850770de29a9e76cd715848aaa566c
kanttouchthis <jonaskonschak@gmx.de>
2021-10-11 18:40:42 +0200

    added folder check

--- clip_search.py
+++ clip_search.py
@@ -12,7 +12,8 @@ from CLIP import clip
 
 start = timer()
 
-parser = argparse.ArgumentParser(description="")
+parser = argparse.ArgumentParser(description="(WIP) A simple tool for searching images "
+                                             "inside a local folder with text/image input using CLIP")
 parser.add_argument("-t",  "--text",        type=str, default="",   help="Text search")
 parser.add_argument("-i",  "--image",       type=str, help="Image search")
 parser.add_argument("-r",  "--results",     type=int, default=5,    help="Number of search results to return")
@@ -25,8 +26,8 @@ parser.add_argument("-in", "--initiate",    action="store_true",  help="Initiate
 parser.add_argument("-c",  "--copy",        action="store_true",  help="Copy images to results folder")
 parser.add_argument("-cr", "--copy_remove", action="store_true",  help="Remove old results from folder")
 args = parser.parse_args()
-assert args.text or args.image
-
+assert args.text != "" or args.image, "Text or Image prompt required"
+assert os.path.isdir(args.folder), f"Folder not found: {args.folder}"
 
 exts = ("jpg", "jpeg", "png", "jfif")
 probs_dict = dict()
@@ -37,9 +38,7 @@ model, preprocess = clip.load("ViT-B/32", device=device)
 print(f"Using Device: {device}")
 
 if args.dict is None:
-    dict_filename = f"{args.folder}_features.pt"
-else:
-    dict_filename = args.dict
+    args.dict = f"{args.folder}_features.pt"
 
 with torch.no_grad():
     cifar100 = CIFAR100(root=os.getcwd() + "/cifar", download=True, train=False)
@@ -57,10 +56,7 @@ with torch.no_grad():
 def image_copy(a_list):
     print("copying")
     if args.copy_remove:
-        try:
-            shutil.rmtree(args.copy_folder)
-        except FileNotFoundError:
-            pass
+        shutil.rmtree(args.copy_folder, ignore_errors=True)
     os.makedirs(args.copy_folder, exist_ok=True)
     for result in range(args.results):
         shutil.copy(f"{args.folder}/{a_list[result][0]}", args.copy_folder + "/")
@@ -85,7 +81,7 @@ def load_dict(init, filename):
     return loaded_dict
 
 
-image_dict = load_dict(args.initiate, dict_filename)
+image_dict = load_dict(args.initiate, args.dict)
 
 new_counter = 0
 with torch.no_grad():
@@ -105,14 +101,14 @@ with torch.no_grad():
                 new_counter += 1
                 
             if new_counter % args.save_every == 0 and new_counter != 0:
-                save_dict(image_dict, dict_filename)
+                save_dict(image_dict, args.dict)
                 
             similarity = (image_features @ target_features.T).softmax(dim=-1)
             print(f" | similarity {similarity[0][0]:.6f}")
             probs_dict[file] = similarity[0][0]
 
 if new_counter != 0:
-    save_dict(image_dict, dict_filename)
+    save_dict(image_dict, args.dict)
 
 heap = heapq.nlargest(args.results, probs_dict, key=probs_dict.get)
 a = sorted({image: probs_dict[image] for image in heap}.items(), key=lambda x: x[1], reverse=True)
